f019170d4113f48f449cba8a8122e0269e697bac
yskimIoTrust <yskim@iotrust.kr>
2023-11-21 15:07:49 +0900

    test app key 변경

--- example/src/config/apiKey.tsx
+++ example/src/config/apiKey.tsx
@@ -3,7 +3,8 @@ import { Platform } from 'react-native'
 export type ItestMode = 'dev' | 'stage' | 'prod'
 
 export const getApiKey = (mode: ItestMode, emailVerify?: boolean) => {
-  return 'ak_dev_Ch5a8a2ylJ1lz0Bb5aGqlxAGoGJ7nMUuHWOVDb8Z6yy'
+  //return 'ak_dev_Ch5a8a2ylJ1lz0Bb5aGqlxAGoGJ7nMUuHWOVDb8Z6yy'
+  return 'test_app_key'
 }
 
 export const getApiKeyList = () => {
